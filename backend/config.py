a199727f0bd8c203c9dda9f3687f5028c8939f97
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-11-20 16:27:09 +0800

    feat: email confirmation (#15)
    * feat: configure email sending
    
    * feat: add email confirmation checking
    
    * feat: add token generation, email confirmation service

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_jwt_extended import (
     JWTManager,
 )
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,7 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config["JWT_SECRET_KEY"] = "your_secret_key"
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
@@ -22,7 +23,9 @@ app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
 mail = Mail(app)
