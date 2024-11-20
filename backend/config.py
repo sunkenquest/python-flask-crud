dc82c25e1d386b96b1755874413bc6cb267d9503
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-11-20 10:26:59 +0800

    feat: add email notifation after registration (#13)

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import (
@@ -12,6 +14,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["JWT_SECRET_KEY"] = "your_secret_key"
-jwt = JWTManager(app)
 
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
+
+jwt = JWTManager(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
