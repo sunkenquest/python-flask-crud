9ea37b75f301ca16aac3d8c9e7233d8756f2b692
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-11-22 09:30:15 +0800

    fix: missing env (#21)

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-encryption_key = os.getenv("ENCRYPTION_KEY").encode()
+encryption_key = os.getenv("ENCRYPTION_KEY")
 
 
 def encrypt_password(plain_text_password: str) -> str:
@@ -17,5 +17,5 @@ def encrypt_password(plain_text_password: str) -> str:
 def decrypt_password(encrypted_password: str) -> str:
     """Decrypt the password using the provided encryption key."""
     f = Fernet(encryption_key)
-    decrypted_password = f.decrypt(encrypted_password)
+    decrypted_password = f.decrypt(encrypted_password.encode())
     return decrypted_password.decode()
